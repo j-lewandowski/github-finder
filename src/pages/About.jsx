3062d90cfec1472385ae712eb55462e23335be3a
Kuba <kuba@Kubas-MacBook-Air.local>
2023-05-30 13:10:37 +0200

    added pages and routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit a eum ex
+        tempora repellat numquam libero perspiciatis ab, nulla dolore omnis fuga
+        quis consequuntur aspernatur labore similique magnam illo molestiae
+        necessitatibus nobis et quidem. At atque recusandae saepe commodi, illum
+        dicta nihil amet enim culpa eos veritatis necessitatibus maxime sint
+        unde asperiores delectus, deleniti sunt facere, non esse laboriosam
+        minima tenetur molestiae maiores. Consectetur velit praesentium, rerum
+        suscipit asperiores quos fugit nobis reprehenderit voluptatibus in, id
+        deleniti necessitatibus! Incidunt maiores nihil iusto? Reprehenderit
+        doloremque natus earum neque iure autem labore.
+      </p>
+    </div>
+  );
+}
+
+export default About;
